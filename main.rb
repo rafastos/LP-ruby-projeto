e1078130c132148065063a5645ae230bb900bc47
Rafael S. Lima <rafaelkra2@gmail.com>
2024-10-19 11:28:00 -0300

    [add] função main

--- main.rb
+++ main.rb
@@ -23,3 +23,56 @@ def criar_turma
 
     Turma.new(nome, codigo, professor)
 end
+
+def main
+    gerenciador = GerenciadorTurmas.new
+
+    loop do
+      exibir_menu
+      opcao = gets.chomp.to_i
+
+      case opcao
+      when 1
+        turma = criar_turma
+        gerenciador.adicionar_turma(turma)
+      when 2
+        print "Digite o código da turma a ser removida: "
+        codigo = gets.chomp
+        gerenciador.remover_turma_por_codigo(codigo)
+      when 3
+        print "Digite o nome da turma: "
+        nome_turma = gets.chomp
+        turma = gerenciador.encontrar_turma_por_nome(nome_turma)
+        if turma
+          print "Digite o nome do aluno: "
+          nome_aluno = gets.chomp
+          turma.adicionar_aluno(nome_aluno)
+          gerenciador.salvar_turmas
+          puts "#{nome_aluno} adicionado à turma #{nome_turma} com sucesso!"
+        else
+          puts "Turma não encontrada."
+        end
+      when 4
+        print "Digite o nome da turma: "
+        nome_turma = gets.chomp
+        turma = gerenciador.encontrar_turma_por_nome(nome_turma)
+
+        if turma
+          print "Digite o nome do aluno a ser removido: "
+          nome_aluno = gets.chomp
+          turma.remover_aluno(nome_aluno)
+          gerenciador.salvar_turmas
+          puts "#{nome_aluno} removido da turma #{nome_turma} com sucesso!"
+        else
+          puts "Turma não encontrada."
+        end
+      when 5
+        gerenciador.listar_turmas
+      when 0
+        puts "Encerrando o programa..."
+        break
+      else
+        puts "Opção inválida!"
+      end
+    end
+end
